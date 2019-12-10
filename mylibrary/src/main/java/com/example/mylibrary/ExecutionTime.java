2deb03aebafb58721fe627299a4a36d5b0e3ad4b
wangchang <18211673289@163.com>
2019-12-10 10:46:00 +0800

    第一次提交

---
+++ mylibrary/src/main/java/com/example/mylibrary/ExecutionTime.java
@@ -0,0 +1,18 @@
+package com.example.mylibrary;
+
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+
+/**
+ * author : wangchang
+ * date   : 2019-12-10 10:08
+ * desc   :
+ */
+@Aspect
+public class ExecutionTime {
+
+    @Around("")
+    public void getExecutionTime(){
+
+    }
+}
